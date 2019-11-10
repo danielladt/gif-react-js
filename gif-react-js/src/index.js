c741e4565c9c2e570e52a637988b373ff5d411e9
Daniella <danielladevoto@gmail.com>
2019-11-10 15:59:04 -0500

    need to add btn and better css as well as favorite option

--- gif-react-js/src/index.js
+++ gif-react-js/src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/app.css';
+import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
 import * as serviceWorker from './serviceWorker';
+import request from 'superagent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            gifs: []
+        }
+    }
+
+
+    handleTermChange = (term) => {
+        const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=GZKGwdu6xlIM0iV58yFKJOFLqj0NLXFw`;
+
+        request.get(url, (err, res) => {
+            this.setState({ gifs: res.body.data })
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <SearchBar onTermChange={this.handleTermChange} />
+                <GifList gifs={this.state.gifs} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
